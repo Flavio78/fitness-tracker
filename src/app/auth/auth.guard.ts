ab4ec3e0eb6b7855c6cdec6f3fc0be449de88662
Flavio78 <53538168+Flavio78@users.noreply.github.com>
2022-11-20 18:57:09 +0100

    lecture 110: end

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -2,19 +2,31 @@ import { Injectable } from '@angular/core';
 import {
   ActivatedRouteSnapshot,
   CanActivate,
+  CanLoad,
+  Route,
   Router,
   RouterStateSnapshot,
 } from '@angular/router';
 import { AuthService } from './auth.service';
 
+// CanLoad interface necessary for the canLoad in app-routing.module
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
   constructor(private authService: AuthService, private router: Router) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): boolean {
+    if (this.authService.isAuth()) {
+      return true;
+    } else {
+      this.router.navigate(['/login']);
+      return false;
+    }
+  }
+
+  canLoad(route: Route): boolean {
     if (this.authService.isAuth()) {
       return true;
     } else {
